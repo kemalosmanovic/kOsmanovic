c5cf259747407f09b510da935fdc469b99dc39bd
kemalosmanovic <c0013667@my.shu.ac.uk>
2023-04-03 10:42:24 +0000

    added login and profile

--- itservices/itapps/itapps/settings.py
+++ itservices/itapps/itapps/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'itapps',
     'itreporting.apps.ItreportingConfig',
+    'users',
+    'crispy_forms',
 
 ]
 
@@ -124,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL= 'itreporting-home'
+LOGIN_URL = 'login'
